5776a387a5b0fcacac0441dedd6c8cbfd0e38cc7
DamitusThyYeetus123 <108782125+DamitusThyYeetus123@users.noreply.github.com>
2022-10-11 06:53:32 +1100

    Update main.cpp

--- OS/src/impl/kernel/main.cpp
+++ OS/src/impl/kernel/main.cpp
@@ -1,8 +1,8 @@
 #include "print.h"
 #include "input.h"
 #include "time.h"
-
-extern "c" void kernel_main() {
+extern "c" void kernel_main();
+void kernel_main() {
     print_clear();
     //Set BG Colour
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLUE);
